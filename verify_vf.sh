53a7591d101368e1f9d92e6ffea2a3eef7ac9009
AmichayPolishuk <amichayp@mellanox.com>
2016-03-28 16:36:04 +0300

    Add Bring up all interfaces now working

--- verify_vf.sh
+++ verify_vf.sh
@@ -14,16 +14,13 @@ NUMBER_OF_VFS=$(lspci | grep "Virtual Function" | wc -l)
 
 if [ -n "$NUMBER_OF_VFS" ]; then
     echo "Virtual Functions created successfully !"
+    # Bring Up All Interfaces
+    for port in `ibdev2netdev | cut -d' ' -f5`; do
+        ifconfig $port up
+    done
     exit 0 
 else
     echo "Failed to create virtual functions !"
     exit 1
 fi
 
-# Bring Up All Interfaces
-
-for port in `ibdev2netdev | cut -d' ' -f5`; do
-    ifconfig $port up
-done
-
-sleep 5 
