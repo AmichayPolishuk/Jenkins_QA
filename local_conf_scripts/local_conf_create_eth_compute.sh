e681d54f5183a7631af0b0236dbdd8e3dfe9cd99
AmichayPolishuk <amichayp@mellanox.com>
2016-08-11 09:52:28 +0300

    Add eth local conf

---
+++ local_conf_scripts/local_conf_create_eth_compute.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# Debug Session
+if [ ${DEBUG_TRACE:-0} -gt 0 ]; then
+    set -x
+fi
+
+# Error Handling
+set -eu
+set -o pipefail
+
+echo "========================================="
+echo " Create&Edit local.conf for Compute Node "
+echo "========================================="
+sudo cat > /opt/stack/devstack/local.conf << EOF
+[[local|localrc]]
+MULTI_HOST=1
+ADMIN_PASSWORD=password
+MYSQL_PASSWORD=password
+RABBIT_PASSWORD=password
+SERVICE_PASSWORD=password
+HOST_IP=\$(host \$(hostname) | cut -d ' ' -f4)
+# Logging
+LOGDIR=\${LOGDIR:-/opt/stack/logs}
+LOGFILE=\$LOGDIR/stack.sh.log
+LOG_COLOR=False
+RECLONE=yes
+LOGDAYS=1
+# Keystone
+SERVICE_TOKEN=servicetoken
+Q_PLUGIN=ml2
+Q_AGENT=openvswitch
+Q_ML2_PLUGIN_MECHANISM_DRIVERS=openvswitch,sriovnicswitch
+Q_USE_DEBUG_COMMAND=True
+Q_USE_SECGROUP=True
+ENABLE_TENANT_VLANS=True
+Q_ML2_PLUGIN_TYPE_DRIVERS=vlan
+ENABLE_TENANT_TUNNELS=False
+Q_ML2_TENANT_NETWORK_TYPE=vlan
+PHYSICAL_NETWORK=default
+PHYSICAL_INTERFACE=\${mlnx_port}
+OVS_PHYSICAL_BRIDGE=br-\${PHYSICAL_INTERFACE}
+SERVICE_HOST=${controller_ip_address}
+MYSQL_HOST=${controller_ip_address}
+RABBIT_HOST=${controller_ip_address}
+Q_HOST=${controller_ip_address}
+GLANCE_HOSTPORT=${controller_ip_address}:9292
+NOVA_VNC_ENABLED=True
+NOVNCPROXY_URL="http://${controller_ip_address}:6080/vnc_auto.html"
+VNCSERVER_LISTEN=0.0.0.0
+VNCSERVER_PROXYCLIENT_ADDRESS=\$VNCSERVER_LISTEN
+enable_plugin neutron git://git.openstack.org/openstack/neutron stable/mitaka
+# Services
+ENABLED_SERVICES=n-cpu,q-agt,n-api-meta,q-sriov-agt
+USE_SCREEN=True
+[[post-config|\$NOVA_CONF]]
+[DEFAULT]
+pci_passthrough_whitelist ={"'"address"'":"'"*:'"\${mlnx_dev}"'.*"'","'"physical_network"'":"'"default"'"}
+EOF
+exit 0
+
---
+++ local_conf_scripts/local_conf_create_eth_controller.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+# Debug Session
+if [ ${DEBUG_TRACE:-0} -gt 0 ]; then
+    set -x
+fi
+
+# Error Handling
+set -eu
+set -o pipefail
+
+echo "==========================================================="
+echo " Create&Edit local.conf for Controller-Network-Cinder Node "
+echo "==========================================================="
+sudo cat > /opt/stack/devstack/local.conf << EOF
+[[local|localrc]]
+DOWNLOAD_DEFAULT_IMAGES=False
+IMAGE_URLS="http://10.209.24.107/images/mellanox-ubuntu-xenial-OFED3.3-1.5.0.0.qcow2,"
+MULTI_HOST=1
+ADMIN_PASSWORD=password
+MYSQL_PASSWORD=password
+RABBIT_PASSWORD=password
+SERVICE_PASSWORD=password
+HOST_IP=\$(host \$(hostname) | cut -d ' ' -f4)
+# Logging
+LOGDIR=/opt/stack/logs
+LOGFILE=\$LOGDIR/stack.sh.log
+LOG_COLOR=False
+RECLONE=yes
+LOGDAYS=1
+# Cinder
+VOLUME_BACKING_FILE_SIZE=10000M
+# Neutron
+SERVICE_TOKEN=servicetoken
+Q_PLUGIN=ml2
+Q_ML2_PLUGIN_MECHANISM_DRIVERS=openvswitch,sriovnicswitch        
+Q_AGENT=openvswitch
+Q_USE_DEBUG_COMMAND=False
+Q_USE_SECGROUP=True
+ENABLE_TENANT_VLANS=True
+Q_ML2_PLUGIN_TYPE_DRIVERS=flat,vlan
+ENABLE_TENANT_TUNNELS=False
+Q_ML2_TENANT_NETWORK_TYPE=vlan
+PHYSICAL_NETWORK=default
+PHYSICAL_INTERFACE=\${mlnx_port}
+TENANT_VLAN_RANGE=${vlan_pool_start}:${vlan_pool_end}
+NETWORK_API_EXTENSIONS=dhcp_agent_scheduler,external-net,ext-gw-mode,binding,quotas,agent,l3_agent_scheduler,provider,router,extraroute,security-group
+OVS_PHYSICAL_BRIDGE=br-\${mlnx_port}
+ALLOW_NEUTRON_DB_MIGRATIONS=true
+IP_VERSION=4
+FIXED_RANGE="192.168.1.0/24"
+NETWORK_GATEWAY=192.168.1.1
+PROVIDER_SUBNET_NAME=private_network
+PROVIDER_NETWORK_TYPE=vlan
+PUBLIC_NETWORK_GATEWAY=10.209.24.1
+FLOATING_RANGE=10.209.24.0/22
+Q_FLOATING_ALLOCATION_POOL=start=${floating_allocation_pool_start},end=${floating_allocation_pool_end}
+Q_USE_PROVIDERNET_FOR_PUBLIC=True
+PUBLIC_PHYSICAL_NETWORK=public
+OVS_BRIDGE_MAPPINGS=public:br-ex
+Q_ML2_PLUGIN_FLAT_TYPE_OPTIONS=public
+
+disable_service h-eng h-api h-api-cfn h-api-cw n-net n-cpu
+enable_service neutron q-svc q-agt q-dhcp q-l3 q-meta n-novnc n-xvnc n-cauth horizon
+enable_plugin neutron_ml2_mlnx git://github.com/openstack/networking-mlnx 
+enable_plugin neutron git://git.openstack.org/openstack/neutron 
+enable_service tempest
+enable_plugin rally https://github.com/openstack/rally
+USE_SCREEN=True
+
+[[post-config|\$NOVA_CONF]]
+[DEFAULT]
+scheduler_available_filters=nova.scheduler.filters.all_filters
+scheduler_default_filters = RetryFilter, AvailabilityZoneFilter, RamFilter, ComputeFilter, ComputeCapabilitiesFilter, ImagePropertiesFilter, PciPassthroughFilter
+
+[[post-config|/etc/neutron/plugins/ml2/ml2_conf.ini]]
+[ml2_sriov]
+supported_pci_vendor_devs = 15b3:1004,15b3:1014,15b3:1016
+[[post-config|/etc/cinder/cinder.conf]]
+[DEFAULT]
+enabled_backends = iser
+    
+[iser]
+iscsi_ip_address=1.1.1.1
+iscsi_helper=tgtadm
+iscsi_protocol = iser
+volume_driver = cinder.volume.drivers.lvm.LVMVolumeDriver
+volume_backend_name = iser
+volume_group = stack-volumes
+
+[[post-extra|\$TEMPEST_CONFIG]]
+[network]
+port_vnic_type=direct
+EOF
+exit 0
