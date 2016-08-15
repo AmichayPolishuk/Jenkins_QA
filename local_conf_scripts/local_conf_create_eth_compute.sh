c7e9be3d488d85ec547d7cd9d6f62c8fca8c9f7d
AmichayPolishuk <amichayp@mellanox.com>
2016-08-15 21:57:34 +0300

    fix master

--- local_conf_scripts/local_conf_create_eth_compute.sh
+++ local_conf_scripts/local_conf_create_eth_compute.sh
@@ -48,7 +48,7 @@ NOVA_VNC_ENABLED=True
 NOVNCPROXY_URL="http://${controller_ip_address}:6080/vnc_auto.html"
 VNCSERVER_LISTEN=0.0.0.0
 VNCSERVER_PROXYCLIENT_ADDRESS=\$VNCSERVER_LISTEN
-enable_plugin neutron git://git.openstack.org/openstack/neutron stable/mitaka
+enable_plugin neutron git://git.openstack.org/openstack/neutron 
 # Services
 ENABLED_SERVICES=n-cpu,q-agt,n-api-meta,q-sriov-agt
 USE_SCREEN=True
