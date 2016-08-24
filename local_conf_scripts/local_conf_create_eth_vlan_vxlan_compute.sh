45d3eab2810bd111137b2d484e750dd71c33ac29
Noam <noama@mellanox.com>
2016-08-24 16:31:18 +0300

    Update local_conf_create_eth_vlan_vxlan_compute.sh

--- local_conf_scripts/local_conf_create_eth_vlan_vxlan_compute.sh
+++ local_conf_scripts/local_conf_create_eth_vlan_vxlan_compute.sh
@@ -18,19 +18,24 @@ ADMIN_PASSWORD=password
 MYSQL_PASSWORD=password
 RABBIT_PASSWORD=password
 SERVICE_PASSWORD=password
-HOST_IP=$(host $(hostname) | cut -d ' ' -f4)
+HOST_IP=\$(host \$(hostname) | cut -d ' ' -f4)
+
+# Branch's
 NEUTRON_BRANCH=refs/changes/16/275616/5
 NOVA_BRANCH=refs/changes/24/275624/14
+
 # Logging
-LOGDIR=${LOGDIR:-/opt/stack/logs}
-LOGFILE=$LOGDIR/stack.sh.log
+LOGDIR=\${LOGDIR:-/opt/stack/logs}
+LOGFILE=\$LOGDIR/stack.sh.log
 LOG_COLOR=False
 RECLONE=yes
+LOGDAYS=1
+
 # Keystone
 SERVICE_TOKEN=servicetoken
 
 # Neutron
-mlnx_port=`ip link show |grep -a2 vf |head -n1 |awk '{print $2}' |tr -d :`
+mlnx_port=`ip link show |grep -a2 vf |head -n1 |awk '{print \$2}' |tr -d :`
 Q_PLUGIN=ml2
 Q_AGENT=openvswitch
 Q_ML2_PLUGIN_MECHANISM_DRIVERS=openvswitch
@@ -39,10 +44,12 @@ Q_USE_SECGROUP=True
 ENABLE_TENANT_VLANS=True
 Q_ML2_PLUGIN_TYPE_DRIVERS=vxlan,vlan
 Q_ML2_TENANT_NETWORK_TYPE=vxlan,vlan
+OVS_ENABLE_TUNNELING=True
+
+# Interfaces
 PHYSICAL_NETWORK=default
 PHYSICAL_INTERFACE=${mlnx_port}
 OVS_PHYSICAL_BRIDGE=br-${mlnx_port}
-OVS_ENABLE_TUNNELING=True
 TUNNEL_ENDPOINT_IP=10.10.10.3
 
 #
@@ -60,10 +67,10 @@ VNCSERVER_PROXYCLIENT_ADDRESS=$VNCSERVER_LISTEN
 ENABLED_SERVICES=n-cpu,q-agt,n-api-meta
 USE_SCREEN=True
 
-#
-mlnx_dev=`lspci |grep Mell|head -n1|awk '{print $1}' |  sed s/\.0$//g`
+# Extra
+mlnx_dev=`lspci |grep Mell|head -n1|awk '{print \$1}' |  sed s/\.0\$//g`
 [[post-config|$NOVA_CONF]]
 [DEFAULT]
-pci_passthrough_whitelist =[{"'"address"'":"'"*:'"${mlnx_dev}"'.1"'","'"physical_network"'":"null"},{"'"address"'":"'"*:'"${mlnx_dev}"'.2"'","'"physical_network"'":"null"},{"'"address"'":"'"*:'"${mlnx_dev}"'.3"'","'"physical_network"'":"'"default"'"},{"'"address"'":"'"*:'"${mlnx_dev}"'.4"'","'"physical_network"'":"'"default"'"}]
+pci_passthrough_whitelist =[{"'"address"'":"'"*:'"\${mlnx_dev}"'.1"'","'"physical_network"'":"null"},{"'"address"'":"'"*:'"\${mlnx_dev}"'.2"'","'"physical_network"'":"null"},{"'"address"'":"'"*:'"\${mlnx_dev}"'.3"'","'"physical_network"'":"'"default"'"},{"'"address"'":"'"*:'"\${mlnx_dev}"'.4"'","'"physical_network"'":"'"default"'"}]
 EOF
 exit 0
