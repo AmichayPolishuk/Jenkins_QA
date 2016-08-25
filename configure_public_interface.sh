52d447e90fceb1cf1717d9da0021b95e2fd657a7
AmichayPolishuk <amichayp@mellanox.com>
2016-08-25 12:26:33 +0300

    Update configure_public_interface.sh

--- configure_public_interface.sh
+++ configure_public_interface.sh
@@ -10,12 +10,4 @@ set -o pipefail
 
 echo ${public_interface_ip}
 
-#sudo bash -c 'cat << EOF > /etc/sysconfig/network-scripts/ifcfg-br-ex
-#DEVICE=br-ex
-#BOOTPROTO=none
-#ONBOOT=yes
-#NETWORK=10.209.86.0
-#PREFIX=24
-#IPADDR='${public_interface_ip}'
-#EOF'
 sudo ifconfig $public_interface $public_interface_ip/$(echo $floating_range | cut -d'/' -f2)
