523fd2c35bccba24ec8bb4dbdb4c5d6b944073e2
Noam <noama@mellanox.com>
2016-08-25 10:57:18 +0300

    Update local_conf_create_ib_controller.sh

--- local_conf_scripts/local_conf_create_ib_controller.sh
+++ local_conf_scripts/local_conf_create_ib_controller.sh
@@ -15,6 +15,7 @@ cat > /opt/stack/devstack/local.conf <<EOF
 [[local|localrc]]
 DOWNLOAD_DEFAULT_IMAGES=False
 IMAGE_URLS="http://10.209.24.107/images/mellanox-ubuntu-xenial-OFED3.3-1.5.0.0.qcow2,"
+IMAGE_URLS+="http://10.209.24.107/images/mellanox-rhel7.2-OFED3.3-1.5.0.0.qcow2"
 MULTI_HOST=1
 ADMIN_PASSWORD=password
 MYSQL_PASSWORD=password
@@ -26,47 +27,59 @@ HOST_IP=\$(host \$(hostname) | cut -d ' ' -f4)
 LOGDIR=/opt/stack/logs
 LOGFILE=\$LOGDIR/stack.sh.log
 LOG_COLOR=False
-RECLONE=yes
 LOGDAYS=1
+
+# GIT
+RECLONE=no
+
 # Cinder
 VOLUME_BACKING_FILE_SIZE=10000M
+
 # Neutron
 SERVICE_TOKEN=servicetoken
 Q_PLUGIN=ml2
-Q_ML2_PLUGIN_MECHANISM_DRIVERS=openvswitch,mlnx       
+Q_ML2_PLUGIN_MECHANISM_DRIVERS=openvswitch,mlnx,sdnmechdriver    
 Q_AGENT=openvswitch
 Q_USE_DEBUG_COMMAND=False
 Q_USE_SECGROUP=True
 ENABLE_TENANT_VLANS=True
-Q_ML2_PLUGIN_TYPE_DRIVERS=flat,vlan
 ENABLE_TENANT_TUNNELS=False
+Q_ML2_PLUGIN_TYPE_DRIVERS=flat,vlan
 Q_ML2_TENANT_NETWORK_TYPE=vlan
-PHYSICAL_NETWORK=default
-PHYSICAL_INTERFACE=${mlnx_port}
 TENANT_VLAN_RANGE=${vlan_pool_start}:${vlan_pool_end}
+ML2_VLAN_RANGES=default:\$TENANT_VLAN_RANGE
 NETWORK_API_EXTENSIONS=dhcp_agent_scheduler,external-net,ext-gw-mode,binding,quotas,agent,l3_agent_scheduler,provider,router,extraroute,security-group
-OVS_PHYSICAL_BRIDGE=br-${epioib_port}
 ALLOW_NEUTRON_DB_MIGRATIONS=true
+Q_ML2_PLUGIN_FLAT_TYPE_OPTIONS=public
 
+# Networks
 IP_VERSION=4
 FIXED_RANGE="192.168.1.0/24"
 NETWORK_GATEWAY=192.168.1.1
 PROVIDER_SUBNET_NAME=private_network
 PROVIDER_NETWORK_TYPE=vlan
-
-PUBLIC_NETWORK_GATEWAY=10.209.24.1
-FLOATING_RANGE=10.209.24.0/22
-Q_FLOATING_ALLOCATION_POOL=start=${floating_allocation_pool_start},end=${floating_allocation_pool_end}
 Q_USE_PROVIDERNET_FOR_PUBLIC=True
-PUBLIC_PHYSICAL_NETWORK=public
-OVS_BRIDGE_MAPPINGS=public:br-ex,default:br-${epioib_port}
-Q_ML2_PLUGIN_FLAT_TYPE_OPTIONS=public
+PUBLIC_NETWORK_GATEWAY=${public_gateway}
+FLOATING_RANGE=${floating_range}
+Q_FLOATING_ALLOCATION_POOL=start=${floating_allocation_pool_start},end=${floating_allocation_pool_end}
 
+# Interfaces
+PHYSICAL_NETWORK=default
+PHYSICAL_INTERFACE=${epioib_port}
+OVS_PHYSICAL_BRIDGE=br-${epioib_port}
+PUBLIC_PHYSICAL_NETWORK=public
+PUBLIC_INTERFACE=${public_interface}
+PUBLIC_BRIDGE=br-ex
+OVS_BRIDGE_MAPPINGS=default:br-${epioib_port},public:br-ex
+
+# Services
 disable_service h-eng h-api h-api-cfn h-api-cw n-net n-cpu
 enable_service neutron q-svc q-agt q-dhcp q-l3 q-meta n-novnc n-xvnc n-cauth horizon
 enable_service mlnx_dnsmasq
-enable_plugin neutron_ml2_mlnx git://github.com/openstack/networking-mlnx  
 USE_SCREEN=True
+
+# Plugins
+enable_plugin neutron_ml2_mlnx git://github.com/openstack/networking-mlnx
 
 [[post-config|\$NOVA_CONF]]
 [DEFAULT]
