7e20f372df71cde3103cd1d80034ea61813d7c57
Noam <noama@mellanox.com>
2016-08-25 10:44:17 +0300

    Update local_conf_create_ib_compute.sh

--- local_conf_scripts/local_conf_create_ib_compute.sh
+++ local_conf_scripts/local_conf_create_ib_compute.sh
@@ -19,14 +19,20 @@ MYSQL_PASSWORD=password
 RABBIT_PASSWORD=password
 SERVICE_PASSWORD=password
 HOST_IP=\$(host \$(hostname) | cut -d ' ' -f4)
+
 # Logging
 LOGDIR=\${LOGDIR:-/opt/stack/logs}
 LOGFILE=\$LOGDIR/stack.sh.log
 LOG_COLOR=False
-RECLONE=yes
 LOGDAYS=1
+
+# GIT
+RECLONE=no
+
 # Keystone
 SERVICE_TOKEN=servicetoken
+
+# Neutron
 Q_PLUGIN=ml2
 Q_AGENT=openvswitch
 Q_ML2_PLUGIN_MECHANISM_DRIVERS=openvswitch,mlnx
@@ -34,11 +40,15 @@ Q_USE_DEBUG_COMMAND=True
 Q_USE_SECGROUP=True
 ENABLE_TENANT_VLANS=True
 Q_ML2_PLUGIN_TYPE_DRIVERS=vlan
-ENABLE_TENANT_TUNNELS=False
 Q_ML2_TENANT_NETWORK_TYPE=vlan
+ENABLE_TENANT_TUNNELS=False
+
+# Interfaces
 PHYSICAL_NETWORK=default
-PHYSICAL_INTERFACE=${mlnx_port}
+PHYSICAL_INTERFACE=${epioib_port}
 OVS_PHYSICAL_BRIDGE=br-${epioib_port}
+
+#
 SERVICE_HOST=${controller_ip_address}
 MYSQL_HOST=${controller_ip_address}
 RABBIT_HOST=${controller_ip_address}
@@ -48,10 +58,15 @@ NOVA_VNC_ENABLED=True
 NOVNCPROXY_URL="http://${controller_ip_address}:6080/vnc_auto.html"
 VNCSERVER_LISTEN=0.0.0.0
 VNCSERVER_PROXYCLIENT_ADDRESS=\$VNCSERVER_LISTEN
-enable_plugin neutron_ml2_mlnx git://github.com/openstack/networking-mlnx
+
 # Services
 ENABLED_SERVICES=n-cpu,q-agt,n-api-meta,mlnx-agt,eswitchd
 USE_SCREEN=True
+
+# Plugins
+enable_plugin neutron_ml2_mlnx git://github.com/openstack/networking-mlnx
+
+# Extra
 [[post-config|\$NOVA_CONF]]
 [DEFAULT]
 pci_passthrough_whitelist ={"'"address"'":"'"*:'"${mlnx_dev}"'.*"'","'"physical_network"'":"'"default"'"}
