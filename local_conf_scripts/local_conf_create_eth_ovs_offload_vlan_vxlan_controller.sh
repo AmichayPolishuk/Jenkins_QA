8008190b18c6c24bba222e7509fb98226e9c2c35
Noam <noama@mellanox.com>
2016-08-25 08:48:47 +0300

    Update local_conf_create_eth_ovs_offload_vlan_vxlan_controller.sh

--- local_conf_scripts/local_conf_create_eth_ovs_offload_vlan_vxlan_controller.sh
+++ local_conf_scripts/local_conf_create_eth_ovs_offload_vlan_vxlan_controller.sh
@@ -21,59 +21,71 @@ ADMIN_PASSWORD=password
 MYSQL_PASSWORD=password
 RABBIT_PASSWORD=password
 SERVICE_PASSWORD=password
-HOST_IP=$(host $(hostname) | cut -d ' ' -f4)
+HOST_IP=\$(host \$(hostname) | cut -d ' ' -f4)
+
+# GIT
+RECLONE=no
 NEUTRON_BRANCH=refs/changes/16/275616/5
 NOVA_BRANCH=refs/changes/24/275624/14
+
 # Logging
 LOGDIR=/opt/stack/logs
 LOGFILE=$LOGDIR/stack.sh.log
 LOG_COLOR=False
-RECLONE=yes
 LOGDAYS=1
+
 # Cinder
 VOLUME_BACKING_FILE_SIZE=10000M
+
 # Neutron
-mlnx_port=`ip link show |grep -a2 vf |head -n1 |awk '{print $2}' |tr -d :`
+mlnx_port=`ip link show |grep -a2 vf |head -n1 |awk '{print \$2}' |tr -d :`
 SERVICE_TOKEN=servicetoken
-Q_PLUGIN=ml2        
+Q_PLUGIN=ml2
 Q_ML2_PLUGIN_MECHANISM_DRIVERS=openvswitch
 Q_AGENT=openvswitch
 Q_USE_DEBUG_COMMAND=False
 Q_USE_SECGROUP=True
 ENABLE_TENANT_VLANS=True
-TENANT_VLAN_RANGE=51:60
 Q_ML2_PLUGIN_TYPE_DRIVERS=flat,vxlan,vlan
 Q_ML2_TENANT_NETWORK_TYPE=vxlan,vlan
-Q_TUNNEL_TYPES=vxlan
-PHYSICAL_NETWORK=default
+TENANT_VLAN_RANGE=${vlan_pool_start}:${vlan_pool_end}
+ML2_VLAN_RANGES=default:$TENANT_VLAN_RANGE
 NETWORK_API_EXTENSIONS=dhcp_agent_scheduler,external-net,ext-gw-mode,binding,quotas,agent,l3_agent_scheduler,provider,router,extraroute,security-group
-OVS_PHYSICAL_BRIDGE=br-${mlnx_port}
 ALLOW_NEUTRON_DB_MIGRATIONS=true
+Q_ML2_PLUGIN_FLAT_TYPE_OPTIONS=public
+Q_TUNNEL_TYPES=vxlan
+OVS_ENABLE_TUNNELING=True
+
+# Networks
 IP_VERSION=4
 FIXED_RANGE="192.168.1.0/24"
 NETWORK_GATEWAY=192.168.1.1
 PROVIDER_SUBNET_NAME=private_network
 PROVIDER_NETWORK_TYPE=vxlan
-PUBLIC_NETWORK_GATEWAY=10.209.86.1
-FLOATING_RANGE=10.209.86.0/24
-Q_FLOATING_ALLOCATION_POOL=start=10.209.86.26,end=10.209.86.35
 Q_USE_PROVIDERNET_FOR_PUBLIC=True
+PUBLIC_NETWORK_GATEWAY=${public_gateway}
+FLOATING_RANGE=${floating_range}
+Q_FLOATING_ALLOCATION_POOL=start=${floating_allocation_pool_start},end=${floating_allocation_pool_end}
+
+
+# Interfaces
+PHYSICAL_NETWORK=default
+PHYSICAL_INTERFACE=${mlnx_port}
+OVS_PHYSICAL_BRIDGE=br-${mlnx_port}
 PUBLIC_PHYSICAL_NETWORK=public
-Q_ML2_PLUGIN_FLAT_TYPE_OPTIONS=public
-PUBLIC_INTERFACE=em2
+PUBLIC_INTERFACE=${public_interface}
 PUBLIC_BRIDGE=br-ex
 OVS_BRIDGE_MAPPINGS=default:br-${mlnx_port},public:br-ex
-ML2_VLAN_RANGES=default:$TENANT_VLAN_RANGE
-PHYSICAL_INTERFACE=${mlnx_port}
-OVS_ENABLE_TUNNELING=True
 TUNNEL_ENDPOINT_IP=10.10.10.1
 
+# Services
 disable_service h-eng h-api h-api-cfn h-api-cw n-net n-cpu
 enable_service neutron q-svc q-agt q-dhcp q-l3 q-meta n-novnc n-xvnc n-cauth horizon
 enable_service tempest
 USE_SCREEN=True
 
-[[post-config|$NOVA_CONF]]
+# Extra
+[[post-config|\$NOVA_CONF]]
 [DEFAULT]
 scheduler_available_filters=nova.scheduler.filters.all_filters
 scheduler_default_filters = RetryFilter, AvailabilityZoneFilter, RamFilter, ComputeFilter, ComputeCapabilitiesFilter, ImagePropertiesFilter, PciPassthroughFilter
