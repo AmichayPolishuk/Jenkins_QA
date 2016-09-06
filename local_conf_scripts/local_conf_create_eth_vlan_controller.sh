4231de0fce447e84970455d950306d25cf598700
AmichayPolishuk <amichayp@mellanox.com>
2016-09-06 15:54:03 +0300

    Update local_conf_create_eth_vlan_controller.sh

--- local_conf_scripts/local_conf_create_eth_vlan_controller.sh
+++ local_conf_scripts/local_conf_create_eth_vlan_controller.sh
@@ -96,17 +96,18 @@ scheduler_default_filters = RetryFilter, AvailabilityZoneFilter, RamFilter, Comp
 [[post-config|/etc/neutron/plugins/ml2/ml2_conf.ini]]
 [ml2_sriov]
 supported_pci_vendor_devs = 15b3:1004,15b3:1014,15b3:1016
+
 [[post-config|/etc/cinder/cinder.conf]]
 [DEFAULT]
-enabled_backends = iser
-    
-[iser]
+enabled_backends = lvmdriver-1, backend1
+
+[backend1]
 iscsi_ip_address=1.1.1.1
 iscsi_helper=tgtadm
 iscsi_protocol = iser
 volume_driver = cinder.volume.drivers.lvm.LVMVolumeDriver
-volume_backend_name = iser
-volume_group = stack-volumes
+volume_backend_name = backend1
+volume_group = stack-volumes-lvmdriver-1
 
 [[post-extra|\$TEMPEST_CONFIG]]
 [network]
