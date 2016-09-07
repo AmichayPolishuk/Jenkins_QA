879b657263b299601b4586100ce8394fd43d9a56
Noam <noama@mellanox.com>
2016-09-07 16:02:14 +0300

    Update config_vf.sh

--- config_vf.sh
+++ config_vf.sh
@@ -77,6 +77,7 @@ if [ ${MT} == "MT4103" ]; then
     exit 1
 
 elif [ ${MT} == "MT4115" ] || [ ${MT} == "MT4117" ]; then
+    mst start;for dev in `mst status | grep mt411 | awk '{print $1}'`; do mlxconfig -y -d $dev set NUM_OF_VFS=${NUM_OF_VFS}; done
     if [ $FABRIC_TYPE == "InfiniBand" ]; then
     	sed -i  -e 's/E_IPOIB_LOAD=no/E_IPOIB_LOAD=yes/g' /etc/infiniband/openib.conf
     fi
