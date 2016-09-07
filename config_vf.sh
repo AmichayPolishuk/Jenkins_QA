fbd822ac534a9c5a47d1faee150d64c098c86796
Noam <noama@mellanox.com>
2016-09-07 09:20:09 +0300

    Major change - config_vf.sh force nic
    This commit add support for multiple nic's on single machine.

--- config_vf.sh
+++ config_vf.sh
@@ -9,39 +9,65 @@ fi
 set -eu
 set -o pipefail
 
+# Force Nic
+FORCE_NIC=$(FORCE_NIC:-"")
+
+# Number of VF's to bring up
+NUM_OF_VFS=$(NUM_OF_VFS:-4)
+
 # OFED must be installed
 if [ -z "$(ofed_info -s)" ]; then
     exit 1
 fi
 
-HCA=$(ibstat -l | head -1)
+MT=""
+if [ ${FORCE_NIC} != "" ]; then
+    case "${FORCE_NIC}" in
+        CX4)
+            MT=MT4115
+            ;;
+        CX3)
+            MT=MT4103
+            ;;
+        LX)
+            MT=MT4117
+            ;;
+    esac
+fi
+
+if [ ${MT} == "" ]; then
+    HCA=$(sudo ibdev2netdev -v | head -1 | awk '{print $2}')
+    HCA_BUS=$(sudo ibdev2netdev -v | head -1 | awk '{print $1}')
+    MT=$(sudo ibdev2netdev -v | head -1 | awk '{print $3}' | cut -d'(' -f2)
+else
+    HCA=$(sudo ibdev2netdev -v | grep ${MT} | head -1 | awk '{print $2}')
+    HCA_BUS=$(sudo ibdev2netdev -v | grep ${MT} | head -1 | awk '{print $1}')
+fi
+HCA_PORTS=$(sudo ibdev2netdev -v | grep ${MT} | wc -l)
 FABRIC_TYPE=$(ibstat $HCA 1 | grep layer | cut -d' ' -f3)
 
 # ConnectX-3 
-if [ -n "$(lspci | grep ConnectX-3)" ]; then
-
-    # OFED must be installed
-    if [ -z "$(ofed_info -s)" ]; then
-        exit 1
-    fi
+if [ ${MT} == "MT4103" ]; then
     if [ $FABRIC_TYPE == "Ethernet" ]; then
-        HCA_BUS=$(lspci | grep nox | head -1 | cut -d':' -f1)
         # The following line will be inserted to /etc/modprobe.d/mlx4_core.conf
-        ports_number=$(lspci | grep -c "\[ConnectX-3")
-        if [ $ports_number == 1 ]; then
-            CONF_LINE="options mlx4_core port_type_array=2 num_vfs=4 probe_vf=4 enable_64b_cqe_eqe=0 log_num_mgm_entry_size=-1 enable_vfs_qos=1"
+        if [ ${HCA_PORTS} == 1 ]; then
+            CONF_LINE="options mlx4_core port_type_array=2 num_vfs=${NUM_OF_VFS} probe_vf=${NUM_OF_VFS} enable_64b_cqe_eqe=0 log_num_mgm_entry_size=-1 enable_vfs_qos=1"
         fi
-        if [ $ports_number == 2 ]; then
-            CONF_LINE="options mlx4_core port_type_array=2,2 num_vfs=0000:$HCA_BUS:00.0-4;0;0 probe_vf=0000:$HCA_BUS:00.0-4;0;0 enable_64b_cqe_eqe=0 log_num_mgm_entry_size=-1 enable_vfs_qos=1"
+        if [ ${HCA_PORTS} == 2 ]; then
+            CONF_LINE="options mlx4_core port_type_array=2,2 num_vfs=$HCA_BUS-${NUM_OF_VFS};0;0 probe_vf=$HCA_BUS-${NUM_OF_VFS};0;0 enable_64b_cqe_eqe=0 log_num_mgm_entry_size=-1 enable_vfs_qos=1"
         fi
         echo $CONF_LINE > /etc/modprobe.d/mlx4_core.conf
         echo "successfully configured modprobe file"
         exit 0
     fi
-
     if [ $FABRIC_TYPE == "InfiniBand" ]; then
         # The following line will be inserted to /etc/modprobe.d/mlx4_core.conf
-        CONF_LINE="options mlx4_core port_type_array=1,1 num_vfs=4 probe_vf=0 enable_64b_cqe_eqe=0 log_num_mgm_entry_size=-1"
+        if [ ${HCA_PORTS} == 1 ]; then
+            CONF_LINE="options mlx4_core port_type_array=1 num_vfs=${NUM_OF_VFS} probe_vf=0 enable_64b_cqe_eqe=0 log_num_mgm_entry_size=-1"
+        fi
+        if [ ${HCA_PORTS} == 2 ]; then
+            CONF_LINE="options mlx4_core port_type_array=1,1 num_vfs=${NUM_OF_VFS} probe_vf=0 enable_64b_cqe_eqe=0 log_num_mgm_entry_size=-1"
+        fi
         echo $CONF_LINE > /etc/modprobe.d/mlx4_core.conf
         # E_IPOIB_LOAD option in /etc/infiniband/openib.conf will be enabled
         sed -i  -e 's/E_IPOIB_LOAD=no/E_IPOIB_LOAD=yes/g' /etc/infiniband/openib.conf
@@ -50,7 +76,7 @@ if [ -n "$(lspci | grep ConnectX-3)" ]; then
     fi
     exit 1
 
-elif [ -n  "$(lspci | grep ConnectX-4)" ]; then
+elif [ ${MT} == "MT4115" ] || [ ${MT} == "MT4117" ]; then; then
 	if [ $FABRIC_TYPE == "InfiniBand" ]; then
 		sed -i  -e 's/E_IPOIB_LOAD=no/E_IPOIB_LOAD=yes/g' /etc/infiniband/openib.conf
 	fi
@@ -61,12 +87,12 @@ elif [ -n  "$(lspci | grep ConnectX-4)" ]; then
     # Check the Physical Port Number 
     ports_number=$(lspci | grep -c "\[ConnectX-4")
     if [ $ports_number == 1 ]; then
-        crontab -l | { cat; echo "@reboot sleep 60 && echo 4 > /sys/class/infiniband/mlx5_0/device/sriov_numvfs"; } | crontab -
+        crontab -l | { cat; echo "@reboot sleep 60 && echo ${NUM_OF_VFS} > /sys/class/infiniband/mlx5_0/device/sriov_numvfs"; } | crontab -
         exit 0
     fi
     if [ $ports_number == 2 ]; then
-        crontab -l | { cat; echo "@reboot sleep 60 && echo 4 > /sys/class/infiniband/mlx5_0/device/sriov_numvfs"; } | crontab - 
-        crontab -l | { cat; echo "@reboot sleep 60 && echo 4 > /sys/class/infiniband/mlx5_1/device/sriov_numvfs"; } | crontab - 
+        crontab -l | { cat; echo "@reboot sleep 60 && echo ${NUM_OF_VFS} > /sys/class/infiniband/mlx5_0/device/sriov_numvfs"; } | crontab - 
+        crontab -l | { cat; echo "@reboot sleep 60 && echo ${NUM_OF_VFS} > /sys/class/infiniband/mlx5_1/device/sriov_numvfs"; } | crontab - 
         exit 0
     fi
 fi 
